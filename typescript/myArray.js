06b56e34ff4a633a867f6cca5e0f03276cbab3ae
nik551 <nikhil.varghese23@gmail.com>
2025-12-23 10:45:24 +0530

    added for arrays

---
+++ typescript/myArray.js
@@ -0,0 +1,12 @@
+var superHeroes = [];
+// const heroPower: number[] =  [];  
+var heroPower = [];
+superHeroes.push("Spiderman");
+heroPower.push(200);
+var allUsers = [];
+var MLModel = [
+    [255, 255, 255],
+    [234, 255, 255],
+    [200, 255]
+];
+allUsers.push({ name: "Nikhil", isActive: true });
--- typescript/myArray.ts
+++ typescript/myArray.ts
@@ -3,3 +3,16 @@ const superHeroes:string[] =  [];
  const  heroPower: Array<number> =  []; 
 superHeroes.push("Spiderman");
 heroPower.push(200);
+
+type Users={
+    name:string
+    isActive:Boolean;
+}
+const allUsers:Users[] = []
+const MLModel:number[][] =[
+    [255,255,255],
+    [234,255,255],
+    [200,255]
+]
+
+allUsers.push({name:"Nikhil",isActive:true})
